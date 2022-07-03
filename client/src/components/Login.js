3687c72ab9b6689ee21a9f4de28c96b87e2bff59
Sayed Mohammed Nouman <74371107+Skyhero-admin@users.noreply.github.com>
2022-07-03 13:05:53 +0000

    fix: Login is working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 const axios = require("axios").default;
-const url = require("url");
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -8,20 +7,10 @@ const Login = () => {
 
   const LogUserIn = () => {
     console.log(username, pass);
-    let payload = {
-          "user": username,
-          "password": pass
-        };
-    // const params = new url.URLSearchParams(payload);
     axios
       .post("http://127.0.0.1:3001/login", {
-        responseType: "json",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
         user: username,
         password: pass
-        // done
       })
       .then((res) => {
         console.log(res);
